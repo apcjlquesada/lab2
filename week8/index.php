1da96c1d8fbdadf4b649934606fce12d9356303f
Joe Gene Q <jlquesada@apc.edu.phclear>
2024-01-23 08:47:13 +0800

    week 8 PHP lessons

---
+++ week8/index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+<body>
+ 
+<?php
+echo "My first PHP script!";
+?>
+
+
+<?php
+ECHO "Hello World!<br>";
+echo "Hello World!<br>";
+EcHo "Hello World!<br>";
+?>
+
+
+</body>
+</html>
